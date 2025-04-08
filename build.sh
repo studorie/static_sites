6303da4e40c003b6e3b9baaa8575039915052b21
studorie <aleeex2209@gmail.com>
2025-04-08 18:31:24 +0100

    Deploy site to GitHub Pages

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # build.sh - Build the site for GitHub Pages deployment
 
 # Set the basepath to your GitHub repository URL
-BASEPATH="https://github.com/studorie/static_sites/"
+BASEPATH="https://studorie.github.io/static_sites/"
 
 # Run the Python script to generate the site
 python3 src/main.py "$BASEPATH"
